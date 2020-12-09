6e8d65e55735ca041bc01b24d7aef894122df031
Sahal Asghar <sahaldos1@gmail.com>
2020-12-08 19:23:11 -0500

    add navbar and add api image styling

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ function getPokemon() {
     .then((res) => {
       return res.json();
     })
-    .then(data => {
+    .then((data) => {
       console.log(data);
       const pokemon = {
         name: data.name,
         id: data.id,
-        sprite: data.sprites['front_default'],
-        spriteShiny: data.sprites['front_shiny'],
-        type: data.types.map( type => type.type.name).join(', ')
+        sprite: data.sprites["front_default"],
+        spriteShiny: data.sprites["front_shiny"],
+        type: data.types.map((type) => type.type.name).join(", "),
       };
       console.log(pokemon);
       document.getElementById("sprite").src = pokemon.sprite;
       document.getElementById("spriteShiny").src = pokemon.spriteShiny;
-      document.getElementById("name").innerHTML = "name: " + pokemon.name;
-      document.getElementById("idNum").innerHTML = "id: " + pokemon.id;
-      document.getElementById("type").innerHTML = "type: " + pokemon.type;
+      document.getElementById("name").innerHTML = "Name: " + pokemon.name;
+      document.getElementById("idNum").innerHTML = "PokeDex ID: " + pokemon.id;
+      document.getElementById("type").innerHTML = "Type: " + pokemon.type;
     })
-    .catch(error => console.log('ERROR'))
+    .catch((error) => console.log("ERROR"));
 }
